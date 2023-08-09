eecfa248d33cca33e0081b6ad78a4642cf15ac8f
seregakras <seregakras@yandex.ru>
2023-08-09 22:23:03 +0300

    PP_2_1_2_Spring_HelloWorld

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean);
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2);
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat);
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat2);
+        System.out.println(bean == bean2);
+        System.out.println(cat == cat2);
     }
 }
